bf22f24a51051247f9e97cfd9cb861f44f496fef
solvershade <seconduacc07@gmail.com>
2024-09-04 09:06:38 -0400

    removed unused reference to testpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
-import TestPage from './pages/TestPage.js';
 
 /** 
   * Entry point of the application used to manage the pages of the 
@@ -13,7 +12,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/testpage" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
     </>
