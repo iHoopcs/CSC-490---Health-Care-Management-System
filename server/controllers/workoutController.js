a65bc4f94ae93fac73d4e4220f0400f1aa5eb5b7
solvershade <seconduacc07@gmail.com>
2024-09-12 14:03:18 -0400

    now able to generate workouts for each plan, exercises with large machinery are excluded until we ask the user if they have access to a gym or heavy machinery. next up will be finding a place to store workouts in the database.

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -5,9 +5,15 @@ const plans =
   ['casual', 'weight-loss', 'building-muscle']
 
 const exerciseTypes =
-  ['cardio', 'strength', 'strecthing', 'strongman', 'powerlifting', 'ploymetrics'];
+  ['cardio', 'strength', 'stretching', 'strongman', 'powerlifting',
+    'ploymetrics', 'olympic_weightlifting'];
 
 const generateWorkout = async (workoutPlan, req, res) => {
+  if (!plans.includes(workoutPlan)) {
+    console.error(`Invalid workout plan: ${workoutPlan}`);
+    throw new Error(`Invalid workout plan: ${workoutPlan}`);
+  }
+
   try {
     let exerciseData = [];
 
@@ -30,24 +36,52 @@ const generateWorkout = async (workoutPlan, req, res) => {
 }
 
 function filterExercisesByPlan(exerciseData, workoutPlan) {
-  if (workoutPlan = 'casual') {
+  // TODO: since api does not provide reps / time we should provide a duration
+  // for each exercise based on difficulty
+  // TODO: find if user has access to equipment then allow equipment use for
+  // now assume access to equipment excluding large machines
+
+  exerciseData = exerciseData.reduce((unique, exercise) => {
+    return unique.filter(e => e.name === exercise.name).length > 0 ? unique : [...unique, exercise];
+  }, []);
+
+
+  exerciseData = exerciseData.filter(exercise => exercise.equipment != 'machine');
+
+  if (workoutPlan == 'weight-loss') {
     let cardioExercises = exerciseData.filter(exercise => exercise.type === 'cardio');
-    let stretchExercises = exerciseData.filter(exercise => exercise.type === 'stretch');
+    let stretchExercises = exerciseData.filter(exercise => exercise.type === 'stretching');
     let plyometricExercises = exerciseData.filter(exercise => exercise.type === 'plyometric');
 
     let selectedCardio = getRandomElements(cardioExercises, 4);
-    let selectedStretch = getRandomElements(stretchExercises, 3);
+    let selectedStretch = getRandomElements(stretchExercises, 2);
     let selectedPlyometric = getRandomElements(plyometricExercises, 1);
-
-    selectedCardio.forEach(exercise => console.log(exercise.name));
 
     return [...selectedCardio, ...selectedStretch, ...selectedPlyometric];
   }
-  else if (workoutPlan = 'weight-loss') {
+  else if (workoutPlan == 'casual') {
+    let cardioExercises = exerciseData.filter(exercise => exercise.type === 'cardio');
+    let strengthExercises = exerciseData.filter(exercise => exercise.type === 'strength');
+    let stretchExercises = exerciseData.filter(exercise => exercise.type === 'stretching');
 
+    let selectedCardio = getRandomElements(cardioExercises, 2);
+    let selectedStretch = getRandomElements(stretchExercises, 2);
+    let selectedStrength = getRandomElements(strengthExercises, 3);
+
+    return [...selectedCardio, ...selectedStretch, ...selectedStrength];
   }
-  else if (workoutPlan = 'building-muscle') {
+  else if (workoutPlan == 'building-muscle') {
+    let strengthExercises = exerciseData.filter(exercise => exercise.type === 'strength');
+    let powerliftingExercises = exerciseData.filter(exercise => exercise.type === 'powerlifting');
+    let olympicExercises = exerciseData.filter(exercise => exercise.type === 'olympic_weightlifting');
+    let stretchExercises = exerciseData.filter(exercise => exercise.type === 'stretching');
 
+    let selectedStretch = getRandomElements(stretchExercises, 2);
+    let selectedStrength = getRandomElements(strengthExercises, 3);
+    let selectedPowerlifting = getRandomElements(powerliftingExercises, 2);
+    let selectedOlympic = getRandomElements(olympicExercises, 2);
+
+    return [...selectedStretch, ...selectedStrength, ...selectedPowerlifting, ...selectedOlympic];
   }
 }
 
