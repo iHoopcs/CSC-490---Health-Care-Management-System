b4f273e253d476a07686cb39668539710b349508
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2024-09-12 19:02:37 -0400

    dashboard display cards

--- client/src/components/dashboard-display-card/DashboardDisplayCard.js
+++ client/src/components/dashboard-display-card/DashboardDisplayCard.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-export const DashboardCard = () => {
+export const DashboardCard = (props) => {
+  const { displayText, backgroundImgSrc, textColor, width, height } = props; 
+  const cardStyle = {
+    width: width, 
+    height: height,
+    textAlign: 'left',
+    padding: '10px', 
+    border: 'solid black 2px', 
+    backgroundImage: `url(${backgroundImgSrc})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    color: textColor,
+    padding: '20px'
+  }
+
   return (
     <>
-        <div className='card'>
-            <h5>This is a card for the dashboard</h5>
+        <div className='card' style={cardStyle}>
+            <h5>{displayText}</h5>
         </div>
     </>
   )
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { SideBar } from '../../components/dashboard-sidebar/SideBar.js'; 
+import { DashboardCard } from '../../components/dashboard-display-card/DashboardDisplayCard.js';
 
 
 export const Dashboard = () => {
@@ -35,6 +36,26 @@ export const Dashboard = () => {
             </div>
 
             {/*Dashboard display cards*/}
+            <div className='mt-5'>
+              <div className='row w-100 justify-content-between'>
+                <div className='col-sm-auto'>
+                  <DashboardCard displayText="" backgroundImgSrc="https://www.citypng.com/public/uploads/preview/hd-2023-september-black-calendar-png-704081694602999rmnejirv5i.png" textColor="black" width="50vw" height="60vh"/>
+                </div>
+
+                <div className='col-sm-auto'>
+                  <div className='row mb-5'>
+                    <DashboardCard displayText="Want to connect with others?" backgroundImgSrc="https://cdn5.vectorstock.com/i/1000x1000/62/54/social-chat-logo-vector-31826254.jpg" textColor="black" width="25vw" height="20vh"/>
+                  </div>
+
+                  <div className='row'>
+                    <DashboardCard displayText="Test your knowledge of fitness and nutrition!" backgroundImgSrc="https://d1c337161ud3pr.cloudfront.net/files%2F37cc6b20-de9e-46c0-81d0-178afe222946_Games%20for%20Learning.png" textColor="white" width="25vw" height="20vh"/>
+                  </div>
+                </div>
+              </div>
+              
+
+              
+            </div>
           </div>
         </div>
       </div>
