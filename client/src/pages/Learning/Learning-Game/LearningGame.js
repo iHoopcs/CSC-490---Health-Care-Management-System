82bcaee302343c45bda5a4583b0e99c864f8fdb3
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2024-10-09 00:21:42 -0400

    Quiz Game progress - quiz questions / next & prev buttons

--- client/src/pages/Learning/Learning-Game/LearningGame.js
+++ client/src/pages/Learning/Learning-Game/LearningGame.js
@@ -5,27 +5,54 @@ import Modal from 'react-bootstrap/Modal';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+
+const questionsAndAnswers = [
+  {
+    question: 'Which of the following is considered part of the dairy food group?',
+    choices: ['chicken', 'orange', 'potato', 'cheese'],
+    answer: 'cheese'
+  },
+  {
+    question: 'Which vitamin is known to help maintain/support the immune system?',
+    choices: ['Vitamin C', 'Vitamin D', 'Vitamin B12', 'Vitamin E'],
+    answer: 'Vitamin C'
+  },
+  {
+    question: 'What is a clear sign that you are dehydrated or have not drank enough water?',
+    choices: ['scratchy eyes', 'urine color is dark yellow', 'you are hungry', 'sleepiness'],
+    answer: 'urine color is dark yellow'
+  },
+  {
+    question: '',
+    choices: [],
+    answer: ''
+  },
+]
+
 export const LearningGame = (props) => {
-  const [testKnowledgeModalIsVisible, setTestKnowledgeModalIsVisible] = useState(false); 
-  const [testKnowledgeGame, setTestKnowledgeGame] = useState(false); 
-  const [vocabModalIsVisible, setVocabModalIsVisible] = useState(false); 
-  
+  const [quizMenuIsVisible, setQuizMenuIsVisible] = useState(false); 
+  const [quizGameIsVisible, setQuizGameIsVisible] = useState(false); 
+  const [currentQuizGameQuestionIndex, setCurrentQuizGameIndex] = useState(0); 
+
   //test knowledge modal controls
-  const displayTestKnowledgeModal = () => {
-    setTestKnowledgeModalIsVisible(true) //display test knowledge game INFO modal
+  const displayQuizMenu = () => {
+    setQuizMenuIsVisible(true) //display test knowledge game INFO modal
   }
   const handleNoTestKnowledgeModal = () => {
-    setTestKnowledgeModalIsVisible(false)
+    setQuizMenuIsVisible(false)
   }
   const displayTestKnowledgeGame = () => {
-    setTestKnowledgeGame(true) //display test knowledge GAME
+    setQuizGameIsVisible(true) //display test knowledge GAME
+    setQuizMenuIsVisible(false)
   }
 
-
-  const displayVocabModal = () => {
-    setVocabModalIsVisible(true)
+  const nextQuizGameQuestion = () => { 
+    setCurrentQuizGameIndex((prevIndex) => prevIndex+1)
   }
-
+  const previousQuizGameQuestion = () => {
+    setCurrentQuizGameIndex((prevIndex) => prevIndex-1)
+  }
+  
   return (
     <>
       <div className='container-fluid'>
@@ -41,11 +68,11 @@ export const LearningGame = (props) => {
                   style={{
                     backgroundImage:`url('https://images.wakelet.com/resize?id=t4_tr3NIzXzG1VD4Bnu6H&h=2880&w=3840&q=85')`,
                     }}
-                  onClick={displayTestKnowledgeModal}
+                  onClick={displayQuizMenu}
                 >
                 </div>
                 {/*Test Knowledge Info Modal*/}
-                <Modal {...props} size="xl" aria-labelledby="contained-modal-title-vcenter" centered show={testKnowledgeModalIsVisible} >
+                <Modal {...props} size="xl" aria-labelledby="contained-modal-title-vcenter" centered show={quizMenuIsVisible} >
                   <Modal.Header>
                       <Modal.Title id="contained-modal-title-vcenter" className='w-100 text-center'>
                           Welcome to the 'Test your Knowledge' game!
@@ -76,48 +103,61 @@ export const LearningGame = (props) => {
                           </Col>
 
                           <Col>
-                              <Button onClick={displayTestKnowledgeGame} className='w-100'>Yes</Button>
+                              <Button onClick={displayTestKnowledgeGame} className='w-100'>Let's play!</Button>
                           </Col>
                       </Row>
                   </Modal.Footer>
               </Modal>
               
               {/*Test Knowledge Game*/}
-              <Modal {...props} size="xl" aria-labelledby="contained-modal-title-vcenter" centered show={testKnowledgeGame} >
+              <Modal {...props} size="xl" aria-labelledby="contained-modal-title-vcenter" centered show={quizGameIsVisible}>
                   <Modal.Header>
                       <Modal.Title id="contained-modal-title-vcenter" className='w-100 text-center'>
-                        Question 1
+                        Question {currentQuizGameQuestionIndex+1}
                       </Modal.Title>
                   </Modal.Header>
-                  <Modal.Body className='p-4'>
-                    <Row className=''>
-                      <Col className='text-center'>
-                        <Row><Button>Fruits, Vegetables, Grains, Protein Foods, Dairy</Button></Row>
-                      </Col>
-                    </Row>
+                  <Modal.Body className='p-4 d-flex flex-column justify-content-center align-items-center'>
+                    <Col>
+                      <Row className='mb-4'><h5>{questionsAndAnswers[currentQuizGameQuestionIndex].question}</h5></Row>
+                        {
+                          //display each choice as a clickable button
+                          questionsAndAnswers[currentQuizGameQuestionIndex].choices.map((item, index) => {
+                            return (
+                              <>
+                                <Row key={index} className='mb-3'>
+                                  <button className='btn btn-outline-secondary'>{item}</button>
+                                </Row>
+                              </>
+                            )
+                          })
+                        }
+                    </Col>
                   </Modal.Body>
                   <Modal.Footer>
                       <Row className='w-100 justify-content-around'>
+                        {
+                          currentQuizGameQuestionIndex == 0 
+                            ? <Col>
+                                <Button disabled className='w-100'>Previous</Button>
+                              </Col>
+                            : <Col>
+                                <Button onClick={previousQuizGameQuestion} className='w-100'>Previous</Button>
+                              </Col>
+                        }
                           <Col>
-                              <Button onClick={handleNoTestKnowledgeModal} className='w-100'>No</Button>
-                          </Col>
-
-                          <Col>
-                              <Button onClick={displayTestKnowledgeGame} className='w-100'>Next</Button>
+                              <Button onClick={nextQuizGameQuestion} className='w-100'>Next</Button>
                           </Col>
                       </Row>
                   </Modal.Footer>
               </Modal>
-
-
-              </div>
+            </div>
 
               <div className='col'>
                 <div className='card-style border shadow-lg p-4 rounded'
                 style={{
                   backgroundImage: `url('https://www.mrpict.com/uploads/1/8/7/2/18722690/d7d96bb8-cf65-48cc-b93e-e9bbef63a960_1.png')`
                 }}
-                onClick={displayVocabModal}
+                onClick={() => window.alert('TODO')}
                 >
                 </div>
                 {/*Vocab Game Modal*/}
