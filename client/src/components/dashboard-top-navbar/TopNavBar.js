01bbb85ba64c0b977f18e5fc3ad39fcba22d0ce8
solvershade <seconduacc07@gmail.com>
2024-11-13 09:28:16 -0500

    added user info to top navbar.

--- client/src/components/dashboard-top-navbar/TopNavBar.js
+++ client/src/components/dashboard-top-navbar/TopNavBar.js
@@ -1,19 +1,30 @@
 import React from 'react'
-import { Container, Navbar, Nav  } from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 export const TopNavBar = () => {
+  let message = localStorage.getItem('msg');
+  if (message) {
+    message = message.replace('Welcome', '').replace(/!/g, '').trim();
+    message = `Current User: ${message}`;
+  }
+
   return (
-        <>
-            <Navbar bg="dark" variant="dark" sticky="top" className=" py-4 navbar-header"> 
-                <Container >
-                    <Navbar.Brand href='/dashboard'>NutriFitHealth</Navbar.Brand>
-
-                    <Nav>
-                        <Nav.Link>Learn Fitness & Nutrition</Nav.Link>
-
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
+    <>
+      <Navbar bg="dark" variant="dark" sticky="top" className=" py-4 navbar-header">
+        <Container >
+          <Navbar.Brand href='/dashboard'>NutriFitHealth</Navbar.Brand>
+          <Nav>
+            <Nav.Link>
+              Learn Fitness & Nutrition
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Item>
+              <Nav.Link href="/profile" className="navbar-text">{message}</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
