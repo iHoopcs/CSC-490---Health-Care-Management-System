c936fab343b0bf9dcbe53c5f6d2c0c814d976748
solvershade <seconduacc07@gmail.com>
2024-11-24 18:40:56 -0500

    fixed issue where images were not working in production.

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -6,28 +6,36 @@ const { createClient } = require('pexels');
 const getImage = async (req, res) => {
   try {
     const imageName = req.body.name;
-    console.log(String(PEXELS_API_KEY));
 
-    let myPhotos;
+    if (!imageName) {
+      return res.status(400).json({ error: 'Image name is required' });
+    }
 
-    await fetch(`https://api.pexels.com/v1/search?query=${imageName}&per_page=1`, {
+    const response = await fetch(`https://api.pexels.com/v1/search?query=${imageName}&per_page=1`, {
       headers: {
         Authorization: PEXELS_API_KEY
       }
-    }).then(resp => {
-      return resp.json()
-    })
-      .then(data => {
-        getPhotos(data.photos);
-        myPhotos = data.photos;
-      })
+    });
 
+    if (!response.ok) {
+      throw new Error(`Pexels API error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    getPhotos(data.photos);
+    const myPhotos = data.photos;
 
     return res.json(myPhotos);
   } catch (error) {
-    console.error(error);
+    console.error(`Error fetching images: ${error.message}`);
     return res.status(500).json({ error: 'Unable to fetch images' });
   }
+}
+
+function getPhotos(images) {
+  images.map(image => {
+    console.log(image);
+  });
 }
 
 function getPhotos(images) {
